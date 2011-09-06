1c5ffc61d5ecf3d56860b4e02a12712e8775ac4e
Geoffrey Challen <challen@buffalo.edu>
2011-09-05 21:43:02 -0400

    Getting to the point where we can add assignments.

--- urls.py
+++ urls.py
@@ -4,6 +4,26 @@ from course import settings as course_settings
 from django.conf import settings as site_settings
 import os,urlparse
 
+def loadClass(theclass, offering):
+
+  theclass.university = offering.department.university
+  theclass.department = offering.department
+  theclass.theoffering = offering
+  
+  # 09 Aug 2011 : GWA : Override course attributes with class ones if
+  #               necessary.
+
+  if theclass.title == "":
+    theclass.title = theclass.course.title
+
+  if theclass.keywords == "":
+    theclass.keywords = theclass.course.keywords
+
+  if theclass.summary == "":
+    theclass.summary = theclass.course.summary
+
+  return theclass
+
 urlpatterns = []
 
 if course_settings.FullCourseURLs:
@@ -15,14 +35,15 @@ if course_settings.FullCourseURLs:
 for offering in Offering.on_site.all():
   for link in offering.offeringlink_set.filter(site=site_settings.SITE_ID):
     for theclass in offering.classes.all():
+      theclass = loadClass(theclass, offering)
       if theclass.semester.current == True:
         urlpatterns += patterns('',
                                 (r'^%s' % (link.slug),
                                  include('course.views.classes'), {'theclass': theclass}),
-                                (r'^%s/%s/' % (link.slug, theclass.semester.slug),
+                                (r'^%s/%s' % (link.slug, theclass.semester.slug),
                                  include('course.views.classes'), {'theclass': theclass}))
       else:
         pass
         urlpatterns += patterns('',
-                                r'^%s/%s/' % (link.slug, theclass.semester.slug),
+                                r'^%s/%s' % (link.slug, theclass.semester.slug),
                                 include('course.views.classes'), {'theclass': theclass})
--- views/classes.py
+++ views/classes.py
@@ -5,89 +5,12 @@ from course.models import University, Department, Offering, Semester
 from django.conf.urls.defaults import *
 
 urlpatterns = []
-urlpatterns += patterns('',
-                        (r'/', 'course.views.classes.offeringObjectToSummary'))
+urlpatterns += patterns('course.views.classes',
+                        (r'^/$', 'summary'),
+                        (r'^/summary/$', 'summary'))
 
-def fullUrlToSummary(request, university_slug, department_slug, offering_number, semester_slug=None):
-  university = get_object_or_404(University, slug=university_slug)
-  department = get_object_or_404(Department, slug=department_slug, university=university)
-  offering = get_object_or_404(Offering, number=offering_number, department=department)
-
-  return offeringObjectToSummary(request, offering, semester_slug=semester_slug)
-
-def offeringToClass(offering):
-
-  university = offering.department.university
-  semester = get_object_or_404(Semester, current=True, university=university)
-
-  try:
-    theclass = offering.classes.filter(semester=semester)
-    assert(len(theclass) == 1)
-  except AssertionError:
-    return None
-    raise Http404
-
-  theclass = theclass[0]
-  
-  # 09 Aug 2011 : GWA : Override course attributes with class ones if
-  #               necessary.
-
-  if theclass.title != "":
-    title = theclass.title
-  else:
-    title = theclass.course.title
-
-  if theclass.keywords != "":
-    keywords = theclass.keywords
-  else:
-    keywords = theclass.course.keywords
-
-  if theclass.summary != "":
-    summary = theclass.summary
-  else:
-    summary = theclass.course.summary
-
-def offeringObjectToSummary(request, offering, semester_slug=None):
-
-  university = offering.department.university
-  if semester_slug != None:
-    semester = get_object_or_404(Semester, slug=semester_slug, university=university)
-  else:
-    semester = get_object_or_404(Semester, current=True, university=university)
-
-  try:
-    theclass = offering.classes.filter(semester=semester)
-    assert(len(theclass) == 1)
-  except AssertionError:
-    raise Http404
-
-  theclass = theclass[0]
-  
-  # 09 Aug 2011 : GWA : Override course attributes with class ones if
-  #               necessary.
-
-  if theclass.title != "":
-    title = theclass.title
-  else:
-    title = theclass.course.title
-
-  if theclass.keywords != "":
-    keywords = theclass.keywords
-  else:
-    keywords = theclass.course.keywords
-
-  if theclass.summary != "":
-    summary = theclass.summary
-  else:
-    summary = theclass.course.summary
+def summary(request, theclass):
 
   return render_to_response('class/summary.html', 
-                           {'title': title,
-                            'keywords': keywords,
-                            'summary': summary,
-                            'university': offering.department.university,
-                            'department': offering.department,
-                            'offering': offering,
-                            'semester': semester,
-                            'theclass': theclass},
+                           {'theclass': theclass},
                             context_instance=RequestContext(request))
